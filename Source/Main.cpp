8b4c42b50f7e0353a7f8c7d6baee1eb95470e787
pedro souza <pedro-souza@debian.0>
2025-11-05 22:48:02 -0300

    platforms setup

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -3,12 +3,19 @@
 //
 #undef main
 #include "Game.h"
+#include "Network/Platforms.h"
 
 int main() {
+    if(Platforms::platformNetworkingInit() != 0){
+        exit(EXIT_FAILURE);
+    }
+
     Game game;
     if (game.Initialize()){
         game.RunLoop();
     }
     game.Shutdown();
+
+    Platforms::platformNetworkingCleanup();
     return 0;
 }
